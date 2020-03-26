d4515c93e680c2c32d66d6672d7ec952965ab2d6
gericjhon <workingjgp@gmail.com>
2020-03-26 21:52:06 +0800

    Added Global Statistic

--- script.js
+++ script.js
@@ -61,4 +61,188 @@ function fetchData() {
   });
 }
 
+function fetchData() {
+   fetch("https://covid-193.p.rapidapi.com/statistics?country=philippines",{
+    "method":"GET",
+    "headers" : {
+      "x-rapidapi-host": "covid-193.p.rapidapi.com",
+      "x-rapidapi-key":mykey
+    }
+  }
+  ).then(response => {
+    return response.json()
+  }).then(data => {
+     console.log(data.response);
+    const html = data.response.map(user =>{
+      return `
+      <div class="container ">
+      <h1 class="text-center">${user.country}</h1>
+      <p class="text-center">As of ${user.day} </p>
+
+      <div class="container">
+        <div class="row">
+      <div class="col-sm bg-warning text-dark">
+        <h2 class="text-center">New Cases</h2>
+        <h3 class="text-center">${user.cases.new}</h3>
+      </div>
+      <div class="col-sm bg-danger">
+        <h2 class="text-center">Total Cases</h2>
+        <h3 class="text-center">${user.cases.total}</h3>
+      </div>
+      <div class="col-sm bg-success">
+        <h2 class="text-center">Total Recovered</h2>
+        <h3 class="text-center">${user.cases.recovered}</h3>
+      </div>
+  </div>
+</div>
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-sm bg-secondary">
+    <h2 class="text-center">New Deaths</h2>
+    <h3 class="text-center">${user.deaths.new}</h3>
+    </div>
+    <div class="col-sm bg-danger">
+    <h2 class="text-center">Total Deaths</h2>
+    <h3 class="text-center">${user.deaths.total}</h3>
+    </div>
+   
+  </div>
+</div>
+
+
+      `;
+    }).join('')
+
+    
+    document.querySelector('#app').innerHTML = html
+  }).catch(error =>{
+    console.log(error);
+    
+  });
+}
+
 fetchData();
+
+var mykey = config.MY_KEY;
+
+function fetchData() {
+   fetch("https://covid-193.p.rapidapi.com/statistics?country=philippines",{
+    "method":"GET",
+    "headers" : {
+      "x-rapidapi-host": "covid-193.p.rapidapi.com",
+      "x-rapidapi-key":mykey
+    }
+  }
+  ).then(response => {
+    return response.json()
+  }).then(data => {
+    // console.log(data.response);
+    const html = data.response.map(user =>{
+      return `
+      <div class="container ">
+      <h1 class="text-center">${user.country}</h1>
+      <p class="text-center">As of ${user.day} </p>
+
+      <div class="container">
+        <div class="row">
+      <div class="col-sm bg-warning text-dark">
+        <h2 class="text-center">New Cases</h2>
+        <h3 class="text-center">${user.cases.new}</h3>
+      </div>
+      <div class="col-sm bg-danger">
+        <h2 class="text-center">Total Cases</h2>
+        <h3 class="text-center">${user.cases.total}</h3>
+      </div>
+      <div class="col-sm bg-success">
+        <h2 class="text-center">Total Recovered</h2>
+        <h3 class="text-center">${user.cases.recovered}</h3>
+      </div>
+  </div>
+</div>
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-sm bg-secondary">
+    <h2 class="text-center">New Deaths</h2>
+    <h3 class="text-center">${user.deaths.new}</h3>
+    </div>
+    <div class="col-sm bg-danger">
+    <h2 class="text-center">Total Deaths</h2>
+    <h3 class="text-center">${user.deaths.total}</h3>
+    </div>
+   
+  </div>
+</div>
+
+
+      `;
+    }).join('')
+
+    
+    document.querySelector('#app').innerHTML = html
+  }).catch(error =>{
+    console.log(error);
+    
+  });
+}
+
+function fetchGlobalData() {
+   fetch("https://covid-193.p.rapidapi.com/statistics?country=all",{
+    "method":"GET",
+    "headers" : {
+      "x-rapidapi-host": "covid-193.p.rapidapi.com",
+      "x-rapidapi-key":mykey
+    }
+  }
+  ).then(response => {
+    return response.json()
+  }).then(data => {
+     console.log(data.response[0]);
+     const html = data.response.map(user =>{
+       return `
+       <div class="container ">
+       <h1 class="text-center">Global</h1>
+       <p class="text-center">As of ${user.day} </p>
+
+       <div class="container">
+         <div class="row">
+       <div class="col-sm bg-warning text-dark">
+         <h2 class="text-center">New Cases</h2>
+         <h3 class="text-center">${user.cases.new}</h3>
+       </div>
+       <div class="col-sm bg-danger">
+         <h2 class="text-center">Total Cases</h2>
+         <h3 class="text-center">${user.cases.total}</h3>
+       </div>
+       <div class="col-sm bg-success">
+         <h2 class="text-center">Total Recovered</h2>
+         <h3 class="text-center">${user.cases.recovered}</h3>
+       </div>
+   </div>
+ </div>
+ <br>
+ <div class="container">
+   <div class="row">
+     <div class="col-sm bg-secondary">
+     <h2 class="text-center">New Deaths</h2>
+     <h3 class="text-center">${user.deaths.new}</h3>
+     </div>
+     <div class="col-sm bg-danger">
+     <h2 class="text-center">Total Deaths</h2>
+     <h3 class="text-center">${user.deaths.total}</h3>
+     </div>
+   
+   </div>
+ </div>`;
+     }).join('')
+
+    
+     document.querySelector('#global').innerHTML = html
+  }).catch(error =>{
+    console.log(error);
+    
+  });
+}
+
+fetchGlobalData();
